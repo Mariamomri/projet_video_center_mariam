2a2cba39f3383c93d426521cb71d1b6477c6dcf2
Mariam <mariamomri.dev@gmail.com>
2026-07-27 15:45:39 +0200

    Add Video CRUD templates and form

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +15,17 @@ class VideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('videoLink')
-            ->add('descritpion')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('videoLink', TextType::class, [
+                'label' => 'Lien Vidéo'
+            ])
+            ->add('descritpion', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer'
             ])
         ;
     }
