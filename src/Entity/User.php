bca6a5518e82d336c90ecc59c94860a5bb1ff7ef
Mariam <mariamomri.dev@gmail.com>
2026-07-27 19:49:35 +0200

    Add table name and HasLifecycleCallbacks to User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,6 +13,8 @@ use App\Entity\Traits\Timestampable;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Table(name: "users")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
