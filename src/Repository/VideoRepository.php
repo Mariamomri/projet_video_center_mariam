fe55cc203badd2dcd14bba5bed94c367c2ba85a4
Mariam <mariamomri.dev@gmail.com>
2026-07-28 00:35:28 +0200

    Add pagination method to VideoRepository

--- src/Repository/VideoRepository.php
+++ src/Repository/VideoRepository.php
@@ -6,38 +6,32 @@ use App\Entity\Video;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
 /**
  * @extends ServiceEntityRepository<Video>
  */
 class VideoRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginatorInterface)
     {
         parent::__construct($registry, Video::class);
     }
 
-    //    /**
-    //     * @return Video[] Returns an array of Video objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('v.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * @param int $page
+     * @return PaginationInterface
+     */
 
-    //    public function findOneBySomeField($value): ?Video
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+
+    public function findVideos(int $page): PaginationInterface
+    {
+        $data = $this->createQueryBuilder('v')
+            ->getQuery()
+            ->getResult();
+
+        $videos = $this->paginatorInterface->paginate($data, $page, 9);
+        return $videos;
+    }
 }
