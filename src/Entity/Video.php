301e607da17b54cc20ea2ad0d1c2782448bb431f
Mariam <mariamomri.dev@gmail.com>
2026-07-27 13:43:57 +0200

    Add Video entity with title, videoLink and description fields

---
+++ src/Entity/Video.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VideoRepository::class)]
+class Video
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $videoLink = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $descritpion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getVideoLink(): ?string
+    {
+        return $this->videoLink;
+    }
+
+    public function setVideoLink(string $videoLink): static
+    {
+        $this->videoLink = $videoLink;
+
+        return $this;
+    }
+
+    public function getDescritpion(): ?string
+    {
+        return $this->descritpion;
+    }
+
+    public function setDescritpion(string $descritpion): static
+    {
+        $this->descritpion = $descritpion;
+
+        return $this;
+    }
+}
