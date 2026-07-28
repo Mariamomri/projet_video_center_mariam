7a280d7163407371e3a26b47e426000c47057e11
Mariam <mariamomri.dev@gmail.com>
2026-07-28 18:05:04 +0200

    Add search method to VideoRepository

--- src/Repository/VideoRepository.php
+++ src/Repository/VideoRepository.php
@@ -9,6 +9,8 @@ use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
 
+use App\Model\SearchData;
+
 /**
  * @extends ServiceEntityRepository<Video>
  */
@@ -32,6 +34,30 @@ class VideoRepository extends ServiceEntityRepository
             ->getResult();
 
         $videos = $this->paginatorInterface->paginate($data, $page, 9);
+        return $videos;
+    }
+
+
+
+    /**
+     * @param SearchData $searchData
+     * @return PaginationInterface
+     */
+    public function findBySearch(SearchData $searchData): PaginationInterface
+    {
+        $data = $this->createQueryBuilder('v');
+
+        if (!empty($searchData->q)) {
+            $data = $data
+                ->andWhere('v.title LIKE :q OR v.descritpion LIKE :q')
+                ->setParameter('q', "%{$searchData->q}%");
+        }
+
+        $data = $data
+            ->getQuery()
+            ->getResult();
+
+        $videos = $this->paginatorInterface->paginate($data, $searchData->page, 6);
         return $videos;
     }
 }
