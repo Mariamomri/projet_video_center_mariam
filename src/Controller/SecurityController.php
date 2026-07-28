da17b06bb2df7656ad53dae97776ce4bc41835ea
Mariam <mariamomri.dev@gmail.com>
2026-07-28 22:13:30 +0200

    Translate security controller flash message

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator): Response
     {
 
         if ($this->getUser()) {
-            $this->addFlash('error', 'Vous êtes déjà connecté !');
+            $this->addFlash('error', $translator->trans('You are already logged in !'));
             return $this->redirectToRoute('app_home');
         }
 
