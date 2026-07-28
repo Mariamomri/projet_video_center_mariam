c4168857dd0ef98ff1c3a010f0a810361904ca64
Mariam <mariamomri.dev@gmail.com>
2026-07-29 00:24:02 +0200

    Add translations for video controller messages

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,23 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Video;
+use App\Entity\User;
 use App\Form\VideoType;
+use App\Form\SearchType;
+use App\Model\SearchData;
 use App\Repository\VideoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\User;
-use App\Form\SearchType;
-use App\Model\SearchData;
+
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class VideoController extends AbstractController
 {
     #[Route(path: "/", name: "app_home")]
-    public function index(VideoRepository $repository, Request $request): Response
+    public function index(VideoRepository $repository, TranslatorInterface $translator, Request $request): Response
     {
+        if ($this->getUser()) {
+            /** @var User */
+            $user = $this->getUser();
+            if (!$user->isVerified()) {
+                $this->addFlash("info", $translator->trans("videoController.index.emailNotVerified"));
+            }
+        }
         $searchData = new SearchData();
         $form = $this->createForm(SearchType::class, $searchData);
         $form->handleRequest($request);
@@ -29,16 +37,16 @@ final class VideoController extends AbstractController
             $videos = $repository->findBySearch($searchData);
 
             return $this->render('video/index.html.twig', [
-                'form' => $form,
-                'videos' => $videos
+                'form' => $form->createView(),
+                "videos" => $videos
             ]);
         }
 
         $videos = $repository->findVideos($request->query->getInt('page', 1));
 
         return $this->render('video/index.html.twig', [
-            'form' => $form,
-            'videos' => $videos
+            'form' => $form->createView(),
+            "videos" => $videos
         ]);
     }
 
@@ -50,28 +58,44 @@ final class VideoController extends AbstractController
                 $this->addFlash('error', $translator->trans('You must login to view a Premium Video !'));
                 return $this->redirectToRoute('app_login');
             }
-            /** @var User $user */
+            /** @var User */
             $user = $this->getUser();
             if (!$user->isVerified()) {
-                $this->addFlash('error',  $translator->trans('You must confirm your email to view a Premium Video !'));
+                $this->addFlash('error', $translator->trans('You must confirm your email to view a Premium Video !'));
                 return $this->redirectToRoute('app_home');
             }
         }
 
         return $this->render('video/show.html.twig', [
-            'video' => $video
+            "video" => $video
         ]);
     }
 
 
-    #[Route(path: '/video/{id}/edit', name: 'app_video_edit')]
-    public function edit(Video $video, Request $request, EntityManagerInterface $em): Response
+    #[Route(path: '/video/{id}/edit', name: 'app_video_edit', requirements: ['id' => '\d+'])]
+    public function edit(Video $video, Request $request, EntityManagerInterface $em, TranslatorInterface $translator): Response
     {
+        if ($this->getUser()) {
+            /** @var User */
+            $user = $this->getUser();
+            if (!$user->isVerified()) {
+                $this->addFlash('error', $translator->trans('You must confirm your email to edit Video !'));
+                return $this->redirectToRoute('app_home');
+            } elseif ($video->getUser()->getEmail() !== $user->getEmail()) {
+                $this->addFlash('error', $translator->trans('You must be the user ') . $video->getUser()->getEmail() . $translator->trans(' to edit this video'));
+                return $this->redirectToRoute('app_home');
+            }
+        } else {
+            $this->addFlash('error', $translator->trans('You must login to edit Video !'));
+            return $this->redirectToRoute('app_login');
+        }
+
         $form = $this->createForm(VideoType::class, $video);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
+            $this->addFlash('success', $translator->trans('The video was successfully modified !'));
             return $this->redirectToRoute('app_video_show', ['id' => $video->getId()]);
         }
 
@@ -85,7 +109,14 @@ final class VideoController extends AbstractController
     #[Route(path: '/video/create', name: 'app_video_create')]
     public function create(Request $request, EntityManagerInterface $em, TranslatorInterface $translator): Response
     {
-        if (!$this->getUser()) {
+        if ($this->getUser()) {
+            /** @var User */
+            $user = $this->getUser();
+            if (!$user->isVerified()) {
+                $this->addFlash('error', $translator->trans('You must confirm your email to create Video !'));
+                return $this->redirectToRoute('app_home');
+            }
+        } else {
             $this->addFlash('error', $translator->trans('You must login to create Video !'));
             return $this->redirectToRoute('app_login');
         }
@@ -98,6 +129,7 @@ final class VideoController extends AbstractController
             $video->setUser($this->getUser());
             $em->persist($video);
             $em->flush();
+            $this->addFlash('success', $translator->trans('The video ') . $video->getTitle() . $translator->trans(' was successfully created'));
             return $this->redirectToRoute('app_home');
         }
 
@@ -106,12 +138,27 @@ final class VideoController extends AbstractController
         ]);
     }
 
-
-    #[Route(path: '/video/{id}/delete', name: 'app_video_delete')]
-    public function delete(Video $video, EntityManagerInterface $em): Response
+    #[Route(path: '/video/{id}/delete', name: 'app_video_delete', requirements: ['id' => '\d+'])]
+    public function delete(Video $video, EntityManagerInterface $em, TranslatorInterface $translator): Response
     {
+        if ($this->getUser()) {
+            /** @var User */
+            $user = $this->getUser();
+            if (!$user->isVerified()) {
+                $this->addFlash('error', $translator->trans('You must confirm your email to delete Video !'));
+                return $this->redirectToRoute('app_home');
+            } elseif ($video->getUser()->getEmail() !== $user->getEmail()) {
+                $this->addFlash('error', $translator->trans('You must be the user ') . $video->getUser()->getEmail() . $translator->trans(' to delete this video'));
+                return $this->redirectToRoute('app_home');
+            }
+        } else {
+            $this->addFlash('error', $translator->trans('You must login to delete Video !'));
+            return $this->redirectToRoute('app_login');
+        }
+        $titre = $video->getTitle();
         $em->remove($video);
         $em->flush();
+        $this->addFlash('info', $translator->trans('The video ') . $titre . $translator->trans(' was successfully deleted !'));
         return $this->redirectToRoute('app_home');
     }
 }
