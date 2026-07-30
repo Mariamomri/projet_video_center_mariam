12e4d051d761fa4f0529df1dfc0b51b9de9b6f92
Mariam <mariamomri.dev@gmail.com>
2026-07-30 18:44:25 +0200

    Translate welcome flash message and account controller flashes

--- src/EventListener/LoginSuccessListener.php
+++ src/EventListener/LoginSuccessListener.php
@@ -6,9 +6,12 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoginSuccessListener
 {
+  public function __construct(private TranslatorInterface $translator) {}
+
   #[AsEventListener(event: LoginSuccessEvent::class)]
   public function onLoginSuccess(LoginSuccessEvent $event): void
   {
@@ -19,7 +22,7 @@ class LoginSuccessListener
     $session = $request->getSession();
 
     if ($session instanceof FlashBagAwareSessionInterface) {
-      $session->getFlashBag()->add('info', 'Bienvenue ' . $user->getFirstname() . ' !');
+      $session->getFlashBag()->add('info', $this->translator->trans('Welcome ') . $user->getFirstname() . ' !');
     }
   }
 }
